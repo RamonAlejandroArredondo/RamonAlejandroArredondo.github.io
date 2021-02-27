23f38a90be3abdaf9830cf5785dabad4d399870a
RamonAlejandroArredondo <71475909+RamonAlejandroArredondo@users.noreply.github.com>
2021-02-27 13:45:56 -0800

    Delete Ejemplos Programacion web directory

--- Ejemplos Programacion web/server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res){
-    res.writeHead(200, {'content-Type': 'text/plain'});
-    res.end('Hello World');
-}).listen(3000); 
-
-console.log('Conectado')
